b04ddc68d397c4618d316fc6598c4c4ddaf76654
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2019-04-27 19:48:05 +0900

    add autostart jupyter function

--- titanic/kado/docker/make_container.sh
+++ titanic/kado/docker/make_container.sh
@@ -1,6 +1,6 @@
 cd ../
-docker build -t ml -f docker/DockerFile ./docker/
-docker run -it -v $(pwd):/workdir -w /workdir --name machine_learning ml:3.6 /bin/bash
+docker build -t ml:3.6 -f docker/DockerFile ./docker/
+docker run -it -v $(pwd):/workdir -w /workdir --name machine_learning -p 8000:8000 ml:3.6 /bin/bash
 
 #when --rm is commanded, container will not be made
 #docker run --rm -it -v $(pwd):/workdir -w /workdir ml:3.6 /bin/bash
---
+++ titanic/kado/start_jupyter.sh
@@ -0,0 +1,1 @@
+jupyter notebook --port 8000 --ip=0.0.0.0 --allow-root
